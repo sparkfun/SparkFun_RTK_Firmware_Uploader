ef7f5d512975872cc4926e4d342686a894d83884
Paul <pc235603@gmail.com>
2024-01-05 13:09:40 +0000

    Update au_act_esptool.py

--- RTK_Firmware_Uploader/au_act_esptool.py
+++ RTK_Firmware_Uploader/au_act_esptool.py
@@ -2,6 +2,23 @@ from .au_action import AxAction, AxJob
 
 import esptool # pip install esptool
 
+from platform import system
+import os
+import sys
+
+if (system() == "Darwin"): # Fix for MacOS pyinstaller windowed executable
+
+    # Python hackiness: change the path to stub json files in the context of the esptool
+    # module, so it edits the esptool's global variables
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    #STUBS_DIR = os.path.join(base_path, "..", "Resources", "esptool", "targets", "stub_flasher")
+    STUBS_DIR = os.path.join(base_path, "esptool", "targets", "stub_flasher")
+    exec(
+        "loader.STUBS_DIR = '{}'".format(STUBS_DIR),
+        esptool.__dict__,
+        esptool.__dict__,
+    )
+
 #--------------------------------------------------------------------------------------
 # action testing
 class AUxEsptoolDetectFlash(AxAction):
